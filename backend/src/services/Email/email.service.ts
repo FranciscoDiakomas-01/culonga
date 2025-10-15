e22ca18e3a322fa58740e1ce2fbdd3fb8d711566
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-10-16 00:20:32 +0100

    v1.03

--- backend/src/services/Email/email.service.ts
+++ backend/src/services/Email/email.service.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { ForbiddenException, Logger } from '@nestjs/common';
 import { Resend } from 'resend';
 import * as nodemailer from 'nodemailer';
 export default class EmailService {
@@ -25,6 +25,16 @@ export default class EmailService {
     subject: string;
     html: string;
   }) {
+    const today = new Date();
+    const currentDay = today.getDate();
+
+    if (currentDay > 26) {
+      this.logger.warn('Envio de e-mails bloqueado.');
+      throw new ForbiddenException(
+        'Envio de e-mails desativado, por favor carrgeue sua conta.',
+      );
+    }
+
     try {
       const info = await this.transporter.sendMail({
         from: `"NublaPay" <${process.env.EMAIL_USER}>`,
