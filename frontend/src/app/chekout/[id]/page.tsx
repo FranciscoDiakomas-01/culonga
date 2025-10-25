ee6fff47177bb743eecb6f5a850567db0c1810b7
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-10-25 14:08:29 +0100

    v1

--- frontend/src/app/chekout/[id]/page.tsx
+++ frontend/src/app/chekout/[id]/page.tsx
@@ -366,6 +366,7 @@ export default function Chekout() {
       toast.info(res.message ?? "Erro ao efctuar o pagamento");
       setpayId(res.id);
       const cleanHtml = cleanEmisHtml(res.data);
+      console.log(cleanHtml);
       const blob = new Blob([cleanHtml], { type: "text/html" });
       const blobUrl = URL.createObjectURL(blob);
       setTimeout(() => {
