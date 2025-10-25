ce34d0b75881910611aa7242527da36ef09c3dc1
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-10-26 00:01:36 +0100

    v1

--- backend/src/modules/payments/payments.service.ts
+++ backend/src/modules/payments/payments.service.ts
@@ -69,6 +69,9 @@ export class PaymentsService {
             {
               uuid: data.payid,
             },
+            {
+              paypayCode: data.payid,
+            },
           ],
         },
         include: {
