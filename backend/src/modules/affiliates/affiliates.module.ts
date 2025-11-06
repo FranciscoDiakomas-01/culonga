8634f6f8009cd6829dfb48071238a932b6bc2e89
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-11-06 20:53:51 -0300

    v4

--- backend/src/modules/affiliates/affiliates.module.ts
+++ backend/src/modules/affiliates/affiliates.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AffiliatesService } from './affiliates.service';
 import { AffiliatesController } from './affiliates.controller';
+import DatabaseService from 'src/services/database/database.service';
 
 @Module({
   controllers: [AffiliatesController],
-  providers: [AffiliatesService],
+  providers: [AffiliatesService , DatabaseService],
 })
 export class AffiliatesModule {}
