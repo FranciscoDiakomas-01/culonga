7a1bf0435ee25529631a2b5519717a67e4258215
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-11-06 13:48:19 -0300

    v3

--- backend/src/modules/coupuns/coupuns.module.ts
+++ backend/src/modules/coupuns/coupuns.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoupunsService } from './coupuns.service';
 import { CoupunsController } from './coupuns.controller';
+import DatabaseService from 'src/services/database/database.service';
 
 @Module({
   controllers: [CoupunsController],
-  providers: [CoupunsService],
+  providers: [CoupunsService, DatabaseService],
 })
 export class CoupunsModule {}
