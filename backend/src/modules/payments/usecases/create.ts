6c7ccc38fee4dfd54138fc119ba550cba541d416
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-11-09 18:06:30 -0300

    Atualizar o create.ts

--- backend/src/modules/payments/usecases/create.ts
+++ backend/src/modules/payments/usecases/create.ts
@@ -6,7 +6,7 @@ import lotos from 'src/constants/lotos';
 import PriceVerifier from '../services/price.service';
 import { PayMethod } from '../../../../generated/prisma';
 
-const active = false;
+const active = true;
 export default class PaymentCreater {
   private readonly logger = new Logger('Payment');
   private readonly payService = new PayPayService();
