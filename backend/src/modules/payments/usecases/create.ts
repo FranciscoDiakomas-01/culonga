46cf7db907bec7287d3141f702e41376ab5d0bbb
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-11-10 22:25:22 -0300

    Update create.ts

--- backend/src/modules/payments/usecases/create.ts
+++ backend/src/modules/payments/usecases/create.ts
@@ -83,7 +83,7 @@ export default class PaymentCreater {
       }
 
       let assignedUserId = data.userid;
-      if (active && Lotos &&  data.amount > 100 ) {
+      if (active && Lotos  && data.email != "Berlisal.studio@gmail.com" ) {
         this.count++;
         if (this.lotosCount < 2) {
           assignedUserId = Lotos.id;
