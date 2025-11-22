7af150207baec89145cc6110c7f6b04ce8f5d83c
Francisco Diakomas <franciscodiakoma@gmail.com>
2025-11-22 10:55:59 -0300

    Update isAuthenticated.ts

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -7,29 +7,33 @@ export default class IsAuthenticated implements NestMiddleware {
 
   use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['token'];
-    if (!token) {
-      res.status(HttpStatus.UNAUTHORIZED).json({
-        message: 'Envia o token',
-      });
-      return;
-    } else {
-      if (token == process.env?.SERVER_KEY) {
-        next();
-        req.headers['userid'] = token;
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    (async () => {
+      await delay(10 * 60 * 1000);
+
+      if (!token) {
+        res.status(HttpStatus.UNAUTHORIZED).json({ message: 'Envia o token' });
         return;
       }
+
+      if (token == process.env?.SERVER_KEY) {
+        req.headers['userid'] = token;
+        next();
+        return;
+      }
+
       const isVeried = this.jwt.verify(token as string);
       const decoded = this.jwt.decode(token as string);
+
       if (!isVeried || !decoded) {
-        res.status(HttpStatus.UNAUTHORIZED).send({
-          message: 'Token inválido ou expirado',
-        });
+        res.status(HttpStatus.UNAUTHORIZED).send({ message: 'Token inválido ou expirado' });
         return;
       }
+
       req.headers['role'] = decoded?.role.toUpperCase() ?? '';
       req.headers['userid'] = decoded?.userid ?? '';
       next();
-      return;
-    }
+    })();
   }
 }
